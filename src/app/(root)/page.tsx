1c1502c09d546ea221b5d16cbcf9e3c36faaad5b
MahirAbsar <mahirabsar181@gmail.com>
2025-04-15 13:17:32 +0600

    feat: create product, product card, product price components

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,21 @@
+import ProductList from "@/components/shared/products/product-list";
+import sampleData from "../../../db/sample-data";
+
 export const metadata = {
   title: "Home",
 };
 
 const HomePage = async () => {
-  return <h2 className="text-2xl text-center">Prostore</h2>;
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
